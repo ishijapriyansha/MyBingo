2982756768b285e276f09474ba9bc316d50afcb5
Ishija Priyansha <ishijapriyansha19@gmail.com>
2024-02-16 15:51:12 +0530

    create skeleton project

---
+++ start.js
@@ -0,0 +1,20 @@
+
+        var td = document.getElementsByTagName("td");
+        for (var i = 0; i < td.length; i++) {
+            td[i].onclick = function() {
+           this.classList.add('crossit');
+           }
+           }
+         function added1(){
+            v1=document.getElementById("n1").value;
+            alert(v1);
+            
+         }
+         function added2(){
+            v2=document.getElementById("n2").value;
+            alert(v2);
+
+         }
+         
+           
+    
